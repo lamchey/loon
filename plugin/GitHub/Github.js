c206c030113a610bb46903504c4a7a256d921e76
lamchey <130128886+lamchey@users.noreply.github.com>
2024-12-27 22:38:04 +0800

    Update Github.js

--- plugin/GitHub/Github.js
+++ plugin/GitHub/Github.js
@@ -3,11 +3,11 @@ Loon专用
 2024-12-27
 */
 let githubPrefix = "https://raw.githubusercontent.com/"
-let fastrawPrefix = "https://fastraw.ixnic.net/" //由FastGit群组成员 @duya1234567 提供，代号A镜像。
-let hubinceptPrefix = "https://hub.incept.pw/" // 由FastGit群组成员 @mxe365 提供，代号B镜像。
-// let kkgithubPrefix = "https://raw.kkgithub.com/" //由KGithub提供，暂时失效。
+let A_Prefix = "https://fastraw.ixnic.net/" //由FastGit群组成员 @duya1234567 提供，代号A镜像。
+let B_Prefix = "https://hub.incept.pw/" // 由FastGit群组成员 @mxe365 提供，代号B镜像。
+let C_Prefix = "https://ghp.ci/" 
+let D_Prefix = "https://ghp.lamchey.xyz/" 
 
-//1: fastraw.ixnic.net 2.hub.incept.pw 3.raw.kkgithub.com
 let changeTo = $persistentStore.read("镜像源")
 
 var url = $request.url
@@ -22,13 +22,16 @@ if (!url.startsWith(githubPrefix)) {
 
 if (changeTo == "") {
     headers["host"] = "A镜像"
-    url = url.replace(githubPrefix,fastrawPrefix)
+    url = url.replace(githubPrefix,A_Prefix)
 } else if (changeTo == "B镜像") {
     headers["host"] = "hub.incept.pw"
-    url = url.replace(githubPrefix,hubinceptPrefix)
+    url = url.replace(githubPrefix,B_Prefix)
 } else if (changeTo == "C镜像") {
-    headers["host"] = "raw.kkgithub.com"
-    url = url.replace(githubPrefix,kkgithubPrefix)
+    headers["host"] = "ghp.ci"
+    url = C_Prefix + url
+} else if (changeTo == "D镜像") {
+    headers["host"] = "ghp.lamchey.xyz"
+    url = D_Prefix + url
 }
 
 $done({url:url,headers:headers})
