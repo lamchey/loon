971288e4236f5e015aed4f35ada8e79af41abae4
lamchey <130128886+lamchey@users.noreply.github.com>
2024-12-27 21:55:44 +0800

    Update Github.js

--- plugin/GitHub/Github.js
+++ plugin/GitHub/Github.js
@@ -5,8 +5,8 @@ Loon专用
 let rawGithubPrefix = "https://raw.githubusercontent.com/"
 
 // 镜像源定义
-let ghpciPrefix = "https://ghp.ci/" // 代号A镜像
-let lamcheyPrefix = "https://ghp.lamchey.xyz/" // 由 @lamchey 提供，代号B镜像
+let APrefix = "https://ghp.ci/" // 代号A镜像
+let BPrefix = "https://ghp.lamchey.xyz/" // 由 @lamchey 提供，代号B镜像
 
 // 获取用户选择的镜像源
 let changeTo = $persistentStore.read("镜像源")
@@ -20,16 +20,15 @@ delete headers.Host
 
 // 检查是否为 GitHub raw URL
  if (url.startsWith(rawGithubPrefix)) {
-    // 针对 raw.githubusercontent.com URL 的改写
     if (changeTo == "") {
-        headers["host"] = new URL(ghpciPrefix).host
-        url = ghpciPrefix + url // 拼接前缀
+        headers["host"] = new URL(APrefix).host
+        url = APrefix + url // 拼接前缀
     } else if (changeTo == "A镜像") {
-        headers["host"] = new URL(ghpciPrefix).host
-        url = ghpciPrefix + url
+        headers["host"] = new URL(APrefix).host
+        url = APrefix + url
     }else if (changeTo == "B镜像") {
-        headers["host"] = new URL(lamcheyPrefix).host
-        url = lamcheyPrefix + url
+        headers["host"] = new URL(BPrefix).host
+        url = BPrefix + url
     }
   
 } 
