86b8cc4efbc88341ab8855123883207abc7a030e
lamchey <130128886+lamchey@users.noreply.github.com>
2024-12-27 22:47:02 +0800

    Update Github.js

--- plugin/GitHub/Github.js
+++ plugin/GitHub/Github.js
@@ -5,7 +5,7 @@ Loon专用
 let githubPrefix = "https://raw.githubusercontent.com/"
 let A_Prefix = "https://fastraw.ixnic.net/" //由FastGit群组成员 @duya1234567 提供，代号A镜像。
 let B_Prefix = "https://hub.incept.pw/" // 由FastGit群组成员 @mxe365 提供，代号B镜像。
-let C_Prefix = "https://ghp.ci/" 
+let C_Prefix = "https://ghgo.xyz/" 
 let D_Prefix = "https://ghp.lamchey.xyz/" 
 
 let changeTo = $persistentStore.read("镜像源")
@@ -27,7 +27,7 @@ if (changeTo == "") {
     headers["host"] = "hub.incept.pw"
     url = url.replace(githubPrefix,B_Prefix)
 } else if (changeTo == "C镜像") {
-    headers["host"] = "ghp.ci"
+    headers["host"] = "ghgo.xyz"
     url = C_Prefix + url
 } else if (changeTo == "D镜像") {
     headers["host"] = "ghp.lamchey.xyz"
