7ab12b2bd6e84d2c8804842a1866d8469a845a8f
lamchey <130128886+lamchey@users.noreply.github.com>
2024-12-27 22:59:11 +0800

    Update Github.js

--- plugin/GitHub/Github.js
+++ plugin/GitHub/Github.js
@@ -6,7 +6,6 @@ let githubPrefix = "https://raw.githubusercontent.com/"
 let A_Prefix = "https://fastraw.ixnic.net/" //由FastGit群组成员 @duya1234567 提供，代号A镜像。
 let B_Prefix = "https://hub.incept.pw/" // 由FastGit群组成员 @mxe365 提供，代号B镜像。
 let C_Prefix = "https://ghgo.xyz/" 
-let D_Prefix = "https://ghp.lamchey.xyz/" 
 
 let changeTo = $persistentStore.read("镜像源")
 
@@ -29,9 +28,6 @@ if (changeTo == "") {
 } else if (changeTo == "C镜像") {
     headers["host"] = "ghgo.xyz"
     url = C_Prefix + url
-} else if (changeTo == "D镜像") {
-    headers["host"] = "ghp.lamchey.xyz"
-    url = D_Prefix + url
-}
+} 
 
 $done({url:url,headers:headers})
