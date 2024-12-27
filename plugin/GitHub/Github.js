f73037c0459bfaf5b5beb9d7ce538a74a4c8de7b
lamchey <130128886+lamchey@users.noreply.github.com>
2024-12-27 22:19:12 +0800

    Update Github.js

--- plugin/GitHub/Github.js
+++ plugin/GitHub/Github.js
@@ -1,27 +1,39 @@
 // 镜像源定义
 const mirrorPrefixes = {
-  A镜像: "https://ghp.ci/",
-  B镜像: "https://ghp.lamchey.xyz/"
+  A镜像: "https://fastraw.ixnic.net/", // 由 @duya1234567 提供
+  B镜像: "https://hub.incept.pw/",     // 由 @mxe365 提供
+  C镜像: "https://ghp.ci/",
+  D镜像: "https://ghp.lamchey.xyz/"
 };
 
+// 默认镜像
+const defaultMirror = "A镜像";
+
 // 读取用户选择的镜像源
-const selectedMirror = $persistentStore.read("镜像源") || "A镜像"; // 默认使用 A 镜像
+const selectedMirror = $persistentStore.read("镜像源") || defaultMirror;
 
-// 当前请求的 URL
+// 当前请求的 URL 和头部
 let url = $request.url;
-
-// 获取镜像前缀
-const mirrorPrefix = mirrorPrefixes[selectedMirror] || mirrorPrefixes["A镜像"];
-
-// 检查并重写 URL
-if (mirrorPrefix) {
-  url = `${mirrorPrefix}${url}`;
-}
+let headers = $request.headers;
 
 // 删除原有的 Host 头，防止干扰
-let headers = $request.headers;
 delete headers.host;
 delete headers.Host;
+
+// 检查是否为目标 GitHub URL
+const githubPrefix = "https://raw.githubusercontent.com/";
+if (!url.startsWith(githubPrefix)) {
+  $done({});
+  return;
+}
+
+// 获取镜像前缀
+const mirrorPrefix = mirrorPrefixes[selectedMirror] || mirrorPrefixes[defaultMirror];
+
+// 改写 URL
+url = mirrorPrefix.startsWith("https://ghp.") 
+  ? `${mirrorPrefix}${url}` // 拼接方式
+  : url.replace(githubPrefix, mirrorPrefix); // 替换方式
 
 // 设置新的 Host 头
 headers["host"] = new URL(mirrorPrefix).host;
